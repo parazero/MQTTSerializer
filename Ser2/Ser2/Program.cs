d4e7af5b3ac629c9233f98f465abb095f94b3d10
nadavguy <nadavguy@parazero.com>
2018-04-05 00:26:45 +0300

    Successfully send MQTT message using Infrastructure. In work. Did not commit Certification files.

--- Ser2/Ser2/Program.cs
+++ Ser2/Ser2/Program.cs
@@ -9,14 +9,36 @@ using Ser2.Serializer;
 using Ser2.MessagesStructs;
 using Ser2.Initializers;
 using Ser2.Parser;
+using uPLibrary.Networking.M2Mqtt;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Ser2
 {
     class Program
     {
         const string fileName = "AppSettings.dat";
+
+        /// <summary>
+        /// AWS IoT endpoint - replace with your own
+        /// </summary>
+        private const string IotEndpoint = "a28g8imipibhcw.iot.us-east-1.amazonaws.com";
+        /// <summary>
+        /// TLS1.2 port used by AWS IoT
+        /// </summary>
+        private const int BrokerPort = 8883;
+        /// <summary>
+        /// this must match - partially - what the subscribed is subscribed too
+        /// nicksthings = the THING i created in AWS IoT
+        /// t1/t555 is just an arbitary topic that i'm publishing to. (It needs 2 parts for the rule I'm using to work)
+        /// </summary>
+        private const string Topic = "DeviceStatus";
+
+
         static void Main(string[] args)
         {
+            var publisher = new Program();
+            publisher.Publish();
+
             string Message = "";
 
             Console.WriteLine("Hello World!");
@@ -52,27 +74,28 @@ namespace Ser2
             StatusMessage ParsedStatusMessage = new StatusMessage();
             MessageParser.StatusMessageParser(out ParsedStatusMessage, StatusMessageFieldsIndex, StatusMessageFieldsSize);
 
-            /*
-            Message += EncodeValues(1, (uint)OpCode);
-            Message += EncodeValues(2, (uint)MessageCounter);
-            Message += EncodeValues(4, IDPart1);
-            Message += EncodeValues(4, IDPart2);
-            Message += EncodeValues(4, IDPart3);
-            Message += EncodeValues(1, (uint)TimeSource);
-            Message += EncodeValues(2, (uint)GPSYear);
-            Message += EncodeValues(1, (uint)GPSMonth);
-            Message += EncodeValues(1, (uint)GPSDay);
-            Message += EncodeValues(1, (uint)GPSHour);
-            Message += EncodeValues(1, (uint)GPSMinute);
-            Message += EncodeValues(1, (uint)GPSSecond);
-            Message += EncodeValues(2, (uint)GPSMiliSecond);
-            Message += EncodeValues(1, (uint)Mode);
-            Message += EncodeValues(1, (uint)RTA);
-            Message += EncodeValues(2, (uint)BCS);
-            Message += EncodeValues(1, (uint)IMUStatus);
-            Message += EncodeValues(1, (uint)FDS);
-            Message += EncodeValues(1, (uint)RCChannels);*/
-
+            Message += EncodeValuesAsBytes(1, (uint)2) + ",";
+            Message += EncodeValuesAsBytes(2, (uint)0)+ ",";
+            Message += EncodeValuesAsBytes(4, 300)+ ",";
+            Message += EncodeValuesAsBytes(4, 400)+ ",";
+            Message += EncodeValuesAsBytes(4, 500)+ ",";
+            Message += EncodeValuesAsBytes(1, (uint)2)+ ",";//Accurate
+            Message += EncodeValuesAsBytes(2, (uint)0)+ ",";//Year
+            Message += EncodeValuesAsBytes(1, (uint)0)+ ",";//Month
+            Message += EncodeValuesAsBytes(1, (uint)0)+ ",";//Day
+            Message += EncodeValuesAsBytes(1, (uint)0)+ ",";//Hour
+            Message += EncodeValuesAsBytes(1, (uint)0)+ ",";//Min
+            Message += EncodeValuesAsBytes(1, (uint)0)+ ",";//Sec
+            Message += EncodeValuesAsBytes(2, (uint)0)+ ",";//Mili
+            Message += EncodeValuesAsBytes(1, (uint)6)+ ",";//Mode
+            Message += EncodeValuesAsBytes(1, (uint)1)+ ",";//Ready To Arm
+            Message += EncodeValuesAsBytes(2, (uint)10)+ ",";//Battery LSB=0.1
+            Message += EncodeValuesAsBytes(1, (uint)1)+ ",";//IMU
+            Message += EncodeValuesAsBytes(1, (uint)3)+ ",";//Flash
+            Message += EncodeValuesAsBytes(1, (uint)1);//RC
+            //2,0,0,3,0,0,0,4,0,0,0,5,0,0,0,0  ,0    ,0    ,0   ,0  ,0   ,0  ,0  ,0    ,0    ,0   ,0  ,255 ,0   ,1  ,1  ,3 
+            //2,0,0,3,0,0,0,4,0,0,0,5,0,0,0,Acc,YearL,YearH,Mnth,Day,Hour,Min,Sec,MiliL,MiliH,Mode,RTA,BCSL,BCSH,IMU,FSD,RC
+            //2,0,0,3,0,0,0,4,0,0,0,5,0,0,0,1  ,0    ,0    ,0   ,0  ,0   ,0  ,0  ,0    ,0    ,6   ,1  ,10  ,0   ,1  ,3  ,1
             StatusMessage NewMessage = new StatusMessage();
             NewMessage.OpCode = OpCode;
             NewMessage.MessageCounter = MessageCounter;
@@ -80,13 +103,13 @@ namespace Ser2
             NewMessage.IDPart2 = IDPart2;
             NewMessage.IDPart3 = IDPart3;
             NewMessage.TimeSource = TimeSource;
-            NewMessage.GPSYear = GPSYear;
-            NewMessage.GPSMonth = GPSMonth;
-            NewMessage.GPSDay = GPSDay;
-            NewMessage.GPSHour = GPSHour;
-            NewMessage.GPSMinute = GPSMinute;
-            NewMessage.GPSSecond = GPSSecond;
-            NewMessage.GPSMiliSec = GPSMiliSecond;
+            NewMessage.GPSYear = 2018;
+            NewMessage.GPSMonth = 4;
+            NewMessage.GPSDay = 4;
+            NewMessage.GPSHour = 14;
+            NewMessage.GPSMinute = 5;
+            NewMessage.GPSSecond = 0;
+            NewMessage.GPSMiliSec = 0;
             NewMessage.Mode = Mode;
             NewMessage.RTA = RTA;
             NewMessage.BCS = BCS;
@@ -107,7 +130,7 @@ namespace Ser2
             }
         }
 
-        public static string EncodeValues(int size, uint value)
+        public static string EncodeValuesAsString(int size, uint value)
         {
             byte byte1 = 0;
             byte byte2 = 0;
@@ -161,5 +184,63 @@ namespace Ser2
             return TmpStr;
         }
 
+        public static string EncodeValuesAsBytes(int size, uint value)
+        {
+            byte byte1 = 0;
+            byte byte2 = 0;
+            byte byte3 = 0;
+            byte byte4 = 0;
+            string TmpStr = "";
+            if (size == 1)
+            {
+                byte1 = (byte)(value & 0xFF);
+
+                TmpStr = byte1.ToString();
+            }
+            if (size == 2)
+            {
+                byte1 = (byte)(value & 0xFF);
+                value = (uint)(value >> 8);
+                byte2 = (byte)(value & 0xFF);
+                TmpStr = byte1.ToString() + ","+ byte2.ToString();
+            }
+            if (size == 4)
+            {
+                byte1 = (byte)(value & 0xFF);
+                value = (uint)(value >> 8);
+                byte2 = (byte)(value & 0xFF);
+                value = (uint)(value >> 8);
+                byte3 = (byte)(value & 0xFF);
+                value = (uint)(value >> 8);
+                byte4 = (byte)(value & 0xFF);
+                TmpStr = byte1.ToString() + "," + byte2.ToString() + ","
+                              + byte3.ToString() + "," + byte4.ToString();
+            }
+            return TmpStr;
+        }
+
+        public void Publish()
+        {
+            //convert to pfx using openssl - see confluence
+            //you'll need to add these two files to the project and copy them to the output (not included in source control deliberately!)
+            var clientCert = new X509Certificate2("YOURPFXFILE.pfx", "1");
+            var caCert = X509Certificate.CreateFromSignedFile("rootCA.pem");
+            // create the client
+            var client = new MqttClient(IotEndpoint, BrokerPort, true, caCert, clientCert, MqttSslProtocols.TLSv1_2);
+            //message to publish - could be anything
+            var message = "02c92447002000175135323536363302e20704030e180948000602a100020000";
+            //client naming has to be unique if there was more than one publisher
+            client.Connect("clientid1");
+            //publish to the topic
+            Byte[] A = {2,0,0,44,1,0,0,144,1,0,0,244,1,0,0,2,0,0,0,0,0,0,0,0,0,6,1,200,0,1,3,1} ;
+            client.Publish(Topic, A);
+            //this was in for debug purposes but it's useful to see something in the console
+            if (client.IsConnected)
+            {
+                Console.WriteLine("SUCCESS!");
+            }
+            //wait so that we can see the outcome
+            Console.ReadLine();
+        }
     }
 }
