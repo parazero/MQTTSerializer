d41acc49b714b480ccb4753c60d4e4869d0a35dd
nadavguy <nadavguy@parazero.com>
2018-04-08 21:50:43 +0300

    New Tests added for Message structure & Encoding

--- Ser2/Tests/Test/MessageEncodingTests.cs
+++ Ser2/Tests/Test/MessageEncodingTests.cs
@@ -1,10 +1,109 @@
 ﻿using System;
-namespace Test
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Ser2.MessagesStructs;
+using Ser2.Initializers;
+using Ser2.Serializer;
+
+namespace Test.Encoding
 {
+    [TestClass]
     public class MessageEncodingTests
     {
-        public MessageEncodingTests()
+        [TestMethod]
+        public void StatusMessageEncodingTests()
         {
+            StatusMessage testStatusMessageSize = new StatusMessage();
+            StatusMessage testStatusMessageIndex = new StatusMessage();
+
+            MessageInitializers.StatusMessageFieldSizeInitializer(out testStatusMessageSize);
+            MessageInitializers.StatusMessageFieldIndexInitializer(out testStatusMessageIndex);
+
+            List<Byte> messageValuesList = new List<Byte>();
+
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.OpCode, (uint)2,
+                                                                           messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.MessageCounter,
+                                                                           (uint)0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testStatusMessageSize.IDPart1,
+                                                                           600, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testStatusMessageSize.IDPart2,
+                                                                           700, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testStatusMessageSize.IDPart3,
+                                                                           800, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.TimeSource,
+                                                                           (uint)2, messageValuesList);//Accurate
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSYear,
+                                                                           (uint)0, messageValuesList);//Year
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSMonth,
+                                                                           (uint)0, messageValuesList);//Month
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSDay,
+                                                                           (uint)0, messageValuesList);//Day
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSHour,
+                                                                           (uint)0, messageValuesList);//Hour
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSMinute,
+                                                                           (uint)0, messageValuesList);//Min
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSSecond,
+                                                                           (uint)0, messageValuesList);//Sec
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.GPSMiliSec,
+                                                                           (uint)0, messageValuesList);//Mili
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.Mode,
+                                                                           (uint)4, messageValuesList);//Mode
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.RTA,
+                                                                           (uint)1, messageValuesList);//Ready To Arm
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.BCS,
+                                                                           (uint)160, messageValuesList);//Battery LSB=0.1
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.IMUStatus,
+                                                                           (uint)1, messageValuesList);//IMU
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.FDS,
+                                                                           (uint)3, messageValuesList);//Flash
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testStatusMessageSize.RCChannels,
+                                                                           (uint)1, messageValuesList);//RC
+            //Convert list to Array
+            Byte[] messageinValues = messageValuesList.ToArray();
+            Byte[] ExpectedArray = { 2, 0, 0, 88, 2, 0, 0, 188, 2, 0, 0, 32, 3, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1, 160, 0, 1, 3, 1 };
+            Assert.AreEqual(Convert.ToBase64String(ExpectedArray), Convert.ToBase64String(messageinValues));
         }
+
+        [TestMethod]
+        public void PhysicalMessageEncodingTests()
+        {
+            PhysicalMessage testPhysicalMessageSize = new PhysicalMessage();
+            PhysicalMessage testPhysicalMessageIndex = new PhysicalMessage();
+
+            MessageInitializers.PhysicalMessageFieldSizeInitializer(out testPhysicalMessageSize);
+            MessageInitializers.PhysicalMessageFieldIndexInitializer(out testPhysicalMessageIndex);
+
+            List<Byte> messageValuesList = new List<Byte>();
+
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.OpCode, 3, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.MessageCounter, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testPhysicalMessageSize.IDPart1, 600, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testPhysicalMessageSize.IDPart2, 700, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList((int)testPhysicalMessageSize.IDPart3, 800, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.TimeSource, 2, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSYear, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSMonth, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSDay, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSHour, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSMinute, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSSecond, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSMiliSec, 0, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.NumberOfSatellites, 17, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.Latitude, 32100000, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.Longitude, 35200000, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSAltitude, 3000, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSVelocity, 3, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSAngle, 5, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.GPSGroundSpeed, 15, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.Temperature, 32, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.Humidity, 3000, messageValuesList);
+            messageValuesList = ManualSerializer.EncodeValuesAsBytesInList(testPhysicalMessageSize.UVAUVB, 0, messageValuesList);
+            //Convert list to Array
+            Byte[] messageinValues = messageValuesList.ToArray();
+            Byte[] ExpectedArray = { 3, 0, 0, 88, 2, 0, 0, 188, 2, 0, 0, 32, 3, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 160, 206, 233, 1, 0, 28, 25, 2, 184, 11, 3, 0 , 5, 0 ,15, 0, 32, 0, 184, 11, 0, 0 };
+            Assert.AreEqual(Convert.ToBase64String(ExpectedArray), Convert.ToBase64String(messageinValues));
+        }
+
     }
 }
--- Ser2/Tests/Test/MessageStructureTests.cs
+++ Ser2/Tests/Test/MessageStructureTests.cs
@@ -2,19 +2,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ser2.MessagesStructs;
 using Ser2.Initializers;
 
-namespace Tests
+namespace StructureTests
 {
     [TestClass]
-    public class UnitTest1
+    public class MessageSructureTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicMathTest()
         {
             int a = 16 * 2;
             Assert.AreEqual(32, a);
         }
+
         [TestMethod]
-        public void StatusMessageStructure()
+        public void StatusMessageSize()
         {
             StatusMessage testStatusMessageSize = new StatusMessage();
             MessageInitializers.StatusMessageFieldSizeInitializer(out testStatusMessageSize);
@@ -38,5 +39,196 @@ namespace Tests
                                                          + testStatusMessageSize.RCChannels);
             Assert.AreEqual(32, statusMessageSize);
         }
+
+        [TestMethod]
+        public void StatusMessageVsIndex()
+        {
+            
+            StatusMessage testStatusMessageSize = new StatusMessage();
+            StatusMessage testStatusMessageIndex = new StatusMessage();
+
+            MessageInitializers.StatusMessageFieldSizeInitializer(out testStatusMessageSize);
+            MessageInitializers.StatusMessageFieldIndexInitializer(out testStatusMessageIndex);
+
+
+            int statusMessageSize = (int)(testStatusMessageSize.OpCode + testStatusMessageSize.MessageCounter
+                                                         + testStatusMessageSize.IDPart1
+                                                         + testStatusMessageSize.IDPart2
+                                                         + testStatusMessageSize.IDPart3
+                                                         + testStatusMessageSize.TimeSource
+                                                         + testStatusMessageSize.GPSYear
+                                                         + testStatusMessageSize.GPSMonth
+                                                         + testStatusMessageSize.GPSDay
+                                                         + testStatusMessageSize.GPSHour
+                                                         + testStatusMessageSize.GPSMinute
+                                                         + testStatusMessageSize.GPSSecond
+                                                         + testStatusMessageSize.GPSMiliSec
+                                                         + testStatusMessageSize.Mode
+                                                         + testStatusMessageSize.RTA
+                                                         + testStatusMessageSize.BCS
+                                                         + testStatusMessageSize.IMUStatus
+                                                         + testStatusMessageSize.FDS
+                                                         + testStatusMessageSize.RCChannels);
+            Assert.AreEqual(testStatusMessageIndex.RCChannels, statusMessageSize - testStatusMessageSize.RCChannels);
+        }
+
+        [TestMethod]
+        public void PhysicalMessageSize()
+        {
+            PhysicalMessage testPhysicalMessageSize = new PhysicalMessage();
+            MessageInitializers.PhysicalMessageFieldSizeInitializer(out testPhysicalMessageSize);
+
+            int physicalMessageSize = (int)(testPhysicalMessageSize.OpCode + testPhysicalMessageSize.MessageCounter
+                                            + testPhysicalMessageSize.IDPart1
+                                            + testPhysicalMessageSize.IDPart2
+                                            + testPhysicalMessageSize.IDPart3
+                                            + testPhysicalMessageSize.TimeSource
+                                            + testPhysicalMessageSize.GPSYear
+                                            + testPhysicalMessageSize.GPSMonth
+                                            + testPhysicalMessageSize.GPSDay
+                                            + testPhysicalMessageSize.GPSHour
+                                            + testPhysicalMessageSize.GPSMinute
+                                            + testPhysicalMessageSize.GPSSecond
+                                            + testPhysicalMessageSize.GPSMiliSec
+                                            + testPhysicalMessageSize.NumberOfSatellites
+                                            + testPhysicalMessageSize.Latitude
+                                            + testPhysicalMessageSize.Longitude
+                                            + testPhysicalMessageSize.GPSAltitude
+                                            + testPhysicalMessageSize.GPSVelocity
+                                            + testPhysicalMessageSize.GPSAngle
+                                            + testPhysicalMessageSize.GPSGroundSpeed
+                                            + testPhysicalMessageSize.Temperature
+                                            + testPhysicalMessageSize.Humidity
+                                            + testPhysicalMessageSize.UVAUVB);
+            Assert.AreEqual(48, physicalMessageSize);
+        }
+
+        [TestMethod]
+        public void PhysicalMessageSizeVsIndex()
+        {
+            PhysicalMessage testPhysicalMessageSize = new PhysicalMessage();
+            MessageInitializers.PhysicalMessageFieldSizeInitializer(out testPhysicalMessageSize);
+
+            PhysicalMessage testPhysicalMessageIndex = new PhysicalMessage();
+            MessageInitializers.PhysicalMessageFieldIndexInitializer(out testPhysicalMessageIndex);
+
+            int physicalMessageSize = (int)(testPhysicalMessageSize.OpCode + testPhysicalMessageSize.MessageCounter
+                                            + testPhysicalMessageSize.IDPart1
+                                            + testPhysicalMessageSize.IDPart2
+                                            + testPhysicalMessageSize.IDPart3
+                                            + testPhysicalMessageSize.TimeSource
+                                            + testPhysicalMessageSize.GPSYear
+                                            + testPhysicalMessageSize.GPSMonth
+                                            + testPhysicalMessageSize.GPSDay
+                                            + testPhysicalMessageSize.GPSHour
+                                            + testPhysicalMessageSize.GPSMinute
+                                            + testPhysicalMessageSize.GPSSecond
+                                            + testPhysicalMessageSize.GPSMiliSec
+                                            + testPhysicalMessageSize.NumberOfSatellites
+                                            + testPhysicalMessageSize.Latitude
+                                            + testPhysicalMessageSize.Longitude
+                                            + testPhysicalMessageSize.GPSAltitude
+                                            + testPhysicalMessageSize.GPSVelocity
+                                            + testPhysicalMessageSize.GPSAngle
+                                            + testPhysicalMessageSize.GPSGroundSpeed
+                                            + testPhysicalMessageSize.Temperature
+                                            + testPhysicalMessageSize.Humidity
+                                            + testPhysicalMessageSize.UVAUVB);
+            Assert.AreEqual(testPhysicalMessageIndex.UVAUVB, physicalMessageSize-testPhysicalMessageSize.UVAUVB);
+        }
+
+        [TestMethod]
+        public void KinematiclMessageSize()
+        {
+            KinematicMessage testKinematicMessageSize = new KinematicMessage();
+            MessageInitializers.KinematicMessageFieldSizeInitializer(out testKinematicMessageSize);
+
+            int kinematicMessageSize = (int)(testKinematicMessageSize.OpCode + testKinematicMessageSize.MessageCounter
+                                             + testKinematicMessageSize.IDPart1
+                                             + testKinematicMessageSize.IDPart2
+                                             + testKinematicMessageSize.IDPart3
+                                             + testKinematicMessageSize.TimeSource
+                                             + testKinematicMessageSize.GPSYear
+                                             + testKinematicMessageSize.GPSMonth
+                                             + testKinematicMessageSize.GPSDay
+                                             + testKinematicMessageSize.GPSHour
+                                             + testKinematicMessageSize.GPSMinute
+                                             + testKinematicMessageSize.GPSSecond
+                                             + testKinematicMessageSize.GPSMiliSec
+                                             + testKinematicMessageSize.BaroHeight
+                                             + testKinematicMessageSize.Pressure
+                                             + testKinematicMessageSize.AccX
+                                             + testKinematicMessageSize.AccY
+                                             + testKinematicMessageSize.AccZ
+                                             + testKinematicMessageSize.MagX
+                                             + testKinematicMessageSize.MagY
+                                             + testKinematicMessageSize.MagZ
+                                             + testKinematicMessageSize.GyroX
+                                             + testKinematicMessageSize.GyroY
+                                             + testKinematicMessageSize.GyroZ
+                                             + testKinematicMessageSize.Heading
+                                             + testKinematicMessageSize.Roll
+                                             + testKinematicMessageSize.Pitch
+                                             + testKinematicMessageSize.OrientationX
+                                             + testKinematicMessageSize.OrientationY
+                                             + testKinematicMessageSize.OrientationZ
+                                             + testKinematicMessageSize.OrientationW
+                                             + testKinematicMessageSize.LinearAccX
+                                             + testKinematicMessageSize.LinearAccY
+                                             + testKinematicMessageSize.LinearAccZ
+                                             + testKinematicMessageSize.GravX
+                                             + testKinematicMessageSize.GravY
+                                             + testKinematicMessageSize.GravZ);
+            Assert.AreEqual(73, kinematicMessageSize);
+        }
+
+        [TestMethod]
+        public void KinematiclMessageSizeVsIndex()
+        {
+            KinematicMessage testKinematicMessageSize = new KinematicMessage();
+            MessageInitializers.KinematicMessageFieldSizeInitializer(out testKinematicMessageSize);
+
+            KinematicMessage testKinematicMessageIndex = new KinematicMessage();
+            MessageInitializers.KinematicMessageFieldIndexInitializer(out testKinematicMessageIndex);
+
+            int kinematicMessageSize = (int)(testKinematicMessageSize.OpCode + testKinematicMessageSize.MessageCounter
+                                             + testKinematicMessageSize.IDPart1
+                                             + testKinematicMessageSize.IDPart2
+                                             + testKinematicMessageSize.IDPart3
+                                             + testKinematicMessageSize.TimeSource
+                                             + testKinematicMessageSize.GPSYear
+                                             + testKinematicMessageSize.GPSMonth
+                                             + testKinematicMessageSize.GPSDay
+                                             + testKinematicMessageSize.GPSHour
+                                             + testKinematicMessageSize.GPSMinute
+                                             + testKinematicMessageSize.GPSSecond
+                                             + testKinematicMessageSize.GPSMiliSec
+                                             + testKinematicMessageSize.BaroHeight
+                                             + testKinematicMessageSize.Pressure
+                                             + testKinematicMessageSize.AccX
+                                             + testKinematicMessageSize.AccY
+                                             + testKinematicMessageSize.AccZ
+                                             + testKinematicMessageSize.MagX
+                                             + testKinematicMessageSize.MagY
+                                             + testKinematicMessageSize.MagZ
+                                             + testKinematicMessageSize.GyroX
+                                             + testKinematicMessageSize.GyroY
+                                             + testKinematicMessageSize.GyroZ
+                                             + testKinematicMessageSize.Heading
+                                             + testKinematicMessageSize.Roll
+                                             + testKinematicMessageSize.Pitch
+                                             + testKinematicMessageSize.OrientationX
+                                             + testKinematicMessageSize.OrientationY
+                                             + testKinematicMessageSize.OrientationZ
+                                             + testKinematicMessageSize.OrientationW
+                                             + testKinematicMessageSize.LinearAccX
+                                             + testKinematicMessageSize.LinearAccY
+                                             + testKinematicMessageSize.LinearAccZ
+                                             + testKinematicMessageSize.GravX
+                                             + testKinematicMessageSize.GravY
+                                             + testKinematicMessageSize.GravZ);
+            Assert.AreEqual(testKinematicMessageIndex.GravZ, kinematicMessageSize-testKinematicMessageSize.GravZ);
+        }
+
     }
 }
